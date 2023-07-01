aa1429fcbadab9ea5fcf63fff8e095f9d2cd9171
Iteratum <103502686+Iteratum@users.noreply.github.com>
2023-07-01 16:44:31 +0100

    upgrade

--- src/components/BlogHost.jsx
+++ src/components/BlogHost.jsx
@@ -5,7 +5,7 @@ export const BlogHost = () => {
   return (
     <div className=' bg-gray-400'>
       <div className="mx-auto max-w-2xl lg:mx-0">
-          <h2 className="text-4xl place-items-center lg:place-content-center font-mono tracking-tight sm:text-6xl md:text-8xl lg:text-8xl">News, tips, blogs & insights</h2>
+          <h2 className="text-4xl place-items-center lg:place-items-center font-mono tracking-tight sm:text-6xl md:text-8xl lg:text-8xl">News, tips, blogs & insights</h2>
           <p className="mt-6 text-lg leading-8">
           All latest news about blogs in one place
           </p>
