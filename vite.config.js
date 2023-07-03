23b052fc225c42ee5ba915da7da441ac56d3aa66
Iteratum <103502686+Iteratum@users.noreply.github.com>
2023-07-03 10:30:08 +0100

    fixing 'can't parse json file error'

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-      external: ['@headlessui/react', 'react-router-dom'],
+      external: ['@headlessui/react'],
     }
   }
 })
