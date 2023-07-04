d21ecd32c00babb5f2ecf20da148724eeacadd81
Iteratum <103502686+Iteratum@users.noreply.github.com>
2023-07-04 09:23:14 +0100

    fixing dependency issue

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-      external: ['@headlessui/react', '@heroicons/react/24/outline'],
+      external: [],
     }
   }
 })
