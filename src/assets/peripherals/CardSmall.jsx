f85993e397166bba9b8bb69cf94253c4a2b94747
Iteratum <emmaogbo2002@gmail.com>
2023-07-22 13:58:02 -0700

    making changes to cards

--- src/assets/peripherals/CardSmall.jsx
+++ src/assets/peripherals/CardSmall.jsx
@@ -24,8 +24,8 @@ export const CardSmall = () => {
         <div className="mx-auto min-w-fit px-4 sm:px-6 lg:px-8">
           <div className="mx-auto max-w-2xl py-16 sm:py-18 lg:max-w-none lg:py-18">
             <div className="mt-6 space-y-12 gap-y-6 gap-x-3 grid grid-cols-1 md:grid md:grid-cols-2 md:space-y-0 lg:grid lg:grid-cols-4 lg:space-y-0">
-              {posts.map((post) => post.category == 'Annoucement' &&
-                <div key={post.title} className="group relative bg-cyan-600 flex-wrap pb-3 rounded-lg">
+              {posts.map((post) => post.category == 'Blog' &&
+                <div key={post.title} className="group relative bg-gray-100 flex-wrap pb-3 rounded-lg">
                   <div className="relative h-80 w-full overflow-hidden rounded-lg bg-cyan-800 sm:aspect-h-1 sm:aspect-w-2 lg:aspect-h-1 lg:aspect-w-1 group-hover:opacity-75 sm:h-64">
                     <img
                       src={`https://testapp1-7tdo.onrender.com${post.image}`}
@@ -40,7 +40,7 @@ export const CardSmall = () => {
                         {post.category}
                       </a>
                     </h3>
-                    <p className="text-lg md:text-xl lg:text-2xl font-semibold text-gray-900">{post.title}</p>
+                    <p className="text-base md:text-lg lg:text-xl font-semibold text-gray-900">{post.title}</p>
                   </div>
                 </div>
               )}
